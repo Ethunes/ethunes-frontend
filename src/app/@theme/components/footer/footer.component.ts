6712890d7dc7a87b80518c190ac091de725979dc
canguruhh <sven@mutzl.org>
2021-05-30 16:11:36 +0200

    add github org link

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
       Created by <b><a href="https:/www.ethunes.com" target="_blank">Team ETHunes</a> for the web3weekend</b> 2021
     </span>
     <div class="socials">
-      <a href="#" target="_blank" class="ion ion-social-github"></a>
+      <a href="https://github.com/Ethunes" target="_blank" class="ion ion-social-github"></a>
     </div>
   `,
 })
